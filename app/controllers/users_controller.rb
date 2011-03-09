711efb3f547e94870a297e0363101e34baf9d7b6
Agge Kempff-Andersen <github@kempff-andersen.dk>
2011-03-09 19:20:44 +0100

    a

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = '67016b5d78124108dd56526105414bd1692b5081280d39cfb7fbae9d97bae1be5a35924999d183bbf67a666ca61492b7b6d6f9bccdc763166dd97377ed7ac1d6'
