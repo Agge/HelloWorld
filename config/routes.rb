337c2801569230b07040191f1a3e2034467a311b
Agge Kempff-Andersen <github@kempff-andersen.dk>
2011-03-09 20:40:10 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 TestApp::Application.routes.draw do
-
+  root :to => "users#index"
   resources :users
-
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
